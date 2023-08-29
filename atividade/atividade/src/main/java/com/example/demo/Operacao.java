47690f09f7c04ddf2990b6b67ddc98bf85103e71
Kauan Oliveira das neves <Kauan17neves@gmail.com>
2023-08-29 09:27:06 -0300

    atividade 1 calculadora

---
+++ atividade/atividade/src/main/java/com/example/demo/Operacao.java
@@ -0,0 +1,20 @@
+package com.example.demo;
+
+public class Operacao {
+	public Double soma(Double numeroUm, Double numeroDois) {
+		return numeroUm + numeroDois;	
+	}
+	
+	public Double subtracao(Double numeroUm, Double numeroDois) {
+		return numeroUm - numeroDois;	
+	}
+	
+	public Double multiplicacao(Double numeroUm, Double numeroDois) {
+		return numeroUm - numeroDois;	
+	}
+	
+	public Double divicao(Double numeroUm, Double numeroDois) {
+		return numeroUm - numeroDois;	
+	}
+
+}
