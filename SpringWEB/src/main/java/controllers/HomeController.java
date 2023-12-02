f74581971f5b31cdbb9f9fcaca084a74ebbb8963
Kauan Oliveira das neves <Kauan17neves@gmail.com>
2023-12-02 17:57:13 -0300

    continuacao

--- SpringWEB/src/main/java/controllers/HomeController.java
+++ SpringWEB/src/main/java/controllers/HomeController.java
@@ -5,13 +5,10 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-
 public class HomeController {
-	@GetMapping("/")
-	public String index(Model model) {
-
-		model.addAttribute("nome", "Kauan");
-		return "templates/home/index";
-	}
-
+    @GetMapping("/")
+    public String index(Model model) {
+        model.addAttribute("nome",  "kauan");
+        return "home/index";
+    }
 }
